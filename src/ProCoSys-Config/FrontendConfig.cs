165f251b3d9b526c162c84eeaae09536d05768a2
Henning Bjørgo <HHBJ@equinor.com>
2020-11-27 07:39:10 +0100

    Initial structure.

---
+++ src/ProCoSys-Config/FrontendConfig.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace ProCoSys_Config
+{
+    public static class FrontendConfig
+    {
+        [FunctionName("Frontend")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Processing configuration request...");
+
+            var configConnectionString = Environment.GetEnvironmentVariable("ConnectionStrings:FrontendConfig");
+            var environment = "dev";
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(options =>
+            {
+                options
+                .Connect(configConnectionString)
+                .Select(KeyFilter.Any, LabelFilter.Null)
+                .Select(KeyFilter.Any, environment);
+            });
+            var configuration = builder.Build();
+
+            return new OkObjectResult(configuration.AsEnumerable());
+        }
+    }
+}
