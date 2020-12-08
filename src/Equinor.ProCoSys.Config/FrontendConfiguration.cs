780ace7e49ccd60b2190a216606aad9cbf0ce65c
Henning Bjørgo <HHBJ@equinor.com>
2020-12-08 10:46:12 +0100

    Testing stuiff.

--- src/Equinor.ProCoSys.Config/FrontendConfiguration.cs
+++ src/Equinor.ProCoSys.Config/FrontendConfiguration.cs
@@ -18,12 +18,12 @@ namespace Equinor.ProCoSys.Config
         [Authorize]
         [FunctionName("FrontendConfiguration")]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Frontend/Configuration")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.User, "get", Route = "Frontend/Configuration")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("Processing configuration request...");
-
             ClaimsPrincipal identities = req.HttpContext.User;
+            return new OkObjectResult(identities);
 
             var currentUserEmail = identities?.FindFirstValue(ClaimTypes.Email) ?? string.Empty;
             var currentUserDomain = string.Empty;
